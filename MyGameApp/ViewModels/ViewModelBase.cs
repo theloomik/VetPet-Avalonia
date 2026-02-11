fd5c88e5cb50d21e2621e540ac67e00e9afb142d
LooMik <76074335+theloomik@users.noreply.github.com>
2026-02-11 09:55:12 +0200

    add files

---
+++ MyGameApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace MyGameApp.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ MyGameApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MyGameApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
