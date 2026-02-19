90697daceeb65f156027f859b5351e6bb136ff5d
LooMik <76074335+theloomik@users.noreply.github.com>
2026-02-19 11:06:14 +0200

    Додано функціонал кнопки Новий
    Додано функціонал кнопки Новий, для клієнта, додано модальне вікно, додано для нього заднє затемення+блюр

--- MyGameApp/ViewModels/MainWindowViewModel.cs
+++ MyGameApp/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
-using CommunityToolkit.Mvvm.Input; 
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.EntityFrameworkCore;
 using MyGameApp.Models;
 
@@ -12,20 +12,17 @@ namespace MyGameApp.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-
         private List<Client> _allClients = new();
         private List<Appointment> _allAppointments = new();
         private List<Staff> _allStaff = new();
         private List<Provider> _allProviders = new();
         private List<Stock> _allStock = new();
 
-
         public ObservableCollection<Client> Clients { get; } = new();
         public ObservableCollection<Appointment> Appointments { get; } = new();
         public ObservableCollection<Staff> StaffMembers { get; } = new();
         public ObservableCollection<Provider> Providers { get; } = new();
         public ObservableCollection<Stock> Stocks { get; } = new();
-
 
         private int _selectedIndex;
         public int SelectedIndex
@@ -35,7 +32,6 @@ namespace MyGameApp.ViewModels
             {
                 if (SetProperty(ref _selectedIndex, value))
                 {
-
                     OnPropertyChanged(nameof(SelectedTabName));
                     OnPropertyChanged(nameof(IsClients));
                     OnPropertyChanged(nameof(IsAppointments));
@@ -45,7 +41,7 @@ namespace MyGameApp.ViewModels
 
                     _isSortAscending = true;
                     SortLabel = "A ↓";
-                    
+
                     UpdateList();
                 }
             }
@@ -67,7 +63,6 @@ namespace MyGameApp.ViewModels
         public bool IsProviders => SelectedIndex == 3;
         public bool IsStock => SelectedIndex == 4;
 
-        // пошук
         private string _searchText = "";
         public string SearchText
         {
@@ -81,7 +76,6 @@ namespace MyGameApp.ViewModels
             }
         }
 
-        // сортування
         private string _sortLabel = "A ↓";
         public string SortLabel
         {
@@ -89,16 +83,76 @@ namespace MyGameApp.ViewModels
             set => SetProperty(ref _sortLabel, value);
         }
 
-        private bool _isSortAscending = true; // true = А-Я, false = Я-А
+        private bool _isSortAscending = true;
 
         public IRelayCommand<int> ChangeTabCommand { get; }
         public IRelayCommand ToggleSortCommand { get; }
+        public IRelayCommand OpenAddClientCommand { get; }
+        public IRelayCommand CloseAddClientCommand { get; }
+
+        // -------- МОДАЛКА --------
+
+        public enum AddEntityType
+        {
+            None,
+            Client,
+            Staff,
+            Appointment,
+            Provider,
+            Stock
+        }
+
+        private AddEntityType _currentAddType = AddEntityType.None;
+        public AddEntityType CurrentAddType
+        {
+            get => _currentAddType;
+            set
+            {
+                if (SetProperty(ref _currentAddType, value))
+                {
+                    OnPropertyChanged(nameof(IsAddOpen));
+                    OnPropertyChanged(nameof(BlurRadius));
+                }
+            }
+        }
+
+        public bool IsAddOpen => CurrentAddType != AddEntityType.None;
+        public double BlurRadius => IsAddOpen ? 8 : 0;
+
+        // -------- КОНСТРУКТОР --------
 
         public MainWindowViewModel()
         {
             ChangeTabCommand = new RelayCommand<int>(i => SelectedIndex = i);
             ToggleSortCommand = new RelayCommand(ToggleSort);
-            
+
+            OpenAddClientCommand = new RelayCommand(() =>
+            {
+                switch (SelectedIndex)
+                {
+                    case 0:
+                        CurrentAddType = AddEntityType.Client;
+                        break;
+                    case 1:
+                        CurrentAddType = AddEntityType.Appointment;
+                        break;
+                    case 2:
+                        CurrentAddType = AddEntityType.Staff;
+                        break;
+                    case 3:
+                        CurrentAddType = AddEntityType.Provider;
+                        break;
+                    case 4:
+                        CurrentAddType = AddEntityType.Stock;
+                        break;
+                }
+            });
+
+            CloseAddClientCommand = new RelayCommand(() =>
+            {
+                CurrentAddType = AddEntityType.None;
+            });
+
             _ = InitializeAsync();
         }
 
@@ -115,61 +169,51 @@ namespace MyGameApp.ViewModels
 
             switch (SelectedIndex)
             {
-                case 0: // Клієнти
+                case 0:
                     var qClients = _allClients.Where(c => c.Phone != null && c.Phone.Contains(search));
-                    
-                    if (_isSortAscending)
-                        qClients = qClients.OrderBy(c => c.LastName).ThenBy(c => c.FirstName);
-                    else
-                        qClients = qClients.OrderByDescending(c => c.LastName).ThenByDescending(c => c.FirstName);
+                    qClients = _isSortAscending
+                        ? qClients.OrderBy(c => c.LastName).ThenBy(c => c.FirstName)
+                        : qClients.OrderByDescending(c => c.LastName).ThenByDescending(c => c.FirstName);
 
                     Clients.Clear();
                     foreach (var item in qClients) Clients.Add(item);
                     break;
 
-                case 1: // Записи
+                case 1:
                     var qAppt = _allAppointments.Where(a => a.Client?.Phone != null && a.Client.Phone.Contains(search));
-
-                    if (_isSortAscending)
-                        qAppt = qAppt.OrderBy(a => a.Date);
-                    else
-                        qAppt = qAppt.OrderByDescending(a => a.Date);
+                    qAppt = _isSortAscending
+                        ? qAppt.OrderBy(a => a.Date)
+                        : qAppt.OrderByDescending(a => a.Date);
 
                     Appointments.Clear();
                     foreach (var item in qAppt) Appointments.Add(item);
                     break;
 
-                case 2: // Працівники
+                case 2:
                     var qStaff = _allStaff.Where(s => s.Phone != null && s.Phone.Contains(search));
-
-                    if (_isSortAscending)
-                        qStaff = qStaff.OrderBy(s => s.LastName).ThenBy(s => s.FirstName);
-                    else
-                        qStaff = qStaff.OrderByDescending(s => s.LastName).ThenByDescending(s => s.FirstName);
+                    qStaff = _isSortAscending
+                        ? qStaff.OrderBy(s => s.LastName).ThenBy(s => s.FirstName)
+                        : qStaff.OrderByDescending(s => s.LastName).ThenByDescending(s => s.FirstName);
 
                     StaffMembers.Clear();
                     foreach (var item in qStaff) StaffMembers.Add(item);
                     break;
 
-                case 3: // Провайдери
+                case 3:
                     var qProv = _allProviders.Where(p => p.Name != null && p.Name.ToLower().Contains(search));
-
-                    if (_isSortAscending)
-                        qProv = qProv.OrderBy(p => p.Name);
-                    else
-                        qProv = qProv.OrderByDescending(p => p.Name);
+                    qProv = _isSortAscending
+                        ? qProv.OrderBy(p => p.Name)
+                        : qProv.OrderByDescending(p => p.Name);
 
                     Providers.Clear();
                     foreach (var item in qProv) Providers.Add(item);
                     break;
 
-                case 4: // Склад
+                case 4:
                     var qStock = _allStock.Where(s => s.Medicine?.Name != null && s.Medicine.Name.ToLower().Contains(search));
-
-                    if (_isSortAscending)
-                        qStock = qStock.OrderBy(s => s.Medicine.Name);
-                    else
-                        qStock = qStock.OrderByDescending(s => s.Medicine.Name);
+                    qStock = _isSortAscending
+                        ? qStock.OrderBy(s => s.Medicine.Name)
+                        : qStock.OrderByDescending(s => s.Medicine.Name);
 
                     Stocks.Clear();
                     foreach (var item in qStock) Stocks.Add(item);
@@ -182,36 +226,22 @@ namespace MyGameApp.ViewModels
             try
             {
                 using var db = new VetpetContext();
-                
-                // 1. Завантажуємо Клієнтів
-                var clients = await db.Clients.AsNoTracking().ToListAsync();
-                _allClients = clients; 
 
-                // 2. Завантажуємо Записи
-                var appointments = await db.Appointments
+                _allClients = await db.Clients.AsNoTracking().ToListAsync();
+                _allAppointments = await db.Appointments
                     .AsNoTracking()
                     .Include(a => a.Client)
                     .Include(a => a.Pet)
                     .ToListAsync();
-                _allAppointments = appointments;
-
-                // 3. Завантажуємо Працівників
-                var staff = await db.Staff.AsNoTracking().ToListAsync();
-                _allStaff = staff;
-
-                // 4. Завантажуємо Провайдерів
-                var providers = await db.Providers.AsNoTracking().ToListAsync();
-                _allProviders = providers;
-
-                // 5. Завантажуємо Склад
-                var stocks = await db.Stocks.AsNoTracking().Include(s => s.Medicine).ToListAsync();
-                _allStock = stocks;
+                _allStaff = await db.Staff.AsNoTracking().ToListAsync();
+                _allProviders = await db.Providers.AsNoTracking().ToListAsync();
+                _allStock = await db.Stocks.AsNoTracking().Include(s => s.Medicine).ToListAsync();
 
                 UpdateList();
             }
-            catch (Exception)
+            catch
             {
-
+                // поки без логування
             }
         }
     }
